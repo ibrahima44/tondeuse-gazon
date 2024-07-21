a486ec032973c8c431df4fa2b3ccfc32c045503b
ibrahima <gueyeibou44@yahoo.fr>
2024-07-21 20:28:00 +0200

    first commit

---
+++ src/main/java/tondeuse/gazon/tondeuse/TondeuseApplication.java
@@ -0,0 +1,12 @@
+package tondeuse.gazon.tondeuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TondeuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TondeuseApplication.class, args);
+	}
+}
